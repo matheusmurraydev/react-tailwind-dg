7a0f2ac0c6ef980bbb0ccbd972f1e6c7014f289f
Matheus Murray <2.0turbocoder@gmail.com>
2022-10-22 21:21:25 -0300

    feat(layout): oponente, foto, desafiar

--- src/App.js
+++ src/App.js
@@ -7,9 +7,26 @@ function App() {
       <div className='h-32 bg-white flex justify-center items-center flex-col'>
         <img className='h-28' src={logo}/>
       </div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <p className='text-white text-2xl mt-10 underline'>
+        Oponente
+      </p>
+      <div className='ml-4 mt-4'>
+        <p className='text-white text-2xl'>
+          @mk7.backend
+        </p>
+        <p className='text-white text-2xl'>
+          @jetta.shark
+        </p>
+        <p className='text-white text-2xl'>
+          @125i.sonic.purple
+        </p>
+      </div>
+      <p className='text-white text-2xl mt-10 underline'>
+        Foto
+      </p>
+      <p className='text-white text-2xl mt-10 underline'>
+        Desafiar
+      </p>
     </div>
   );
 }
